458d6c4a4c6ac7df98a8304e4cf27413d9071f0c
amirhy <amirhosein.hosseini76@gmail.com>
2018-11-08 21:57:37 +0330

    first commit form amirhy

---
+++ src/main/java/com/snappad/snappad/SnappadApplication.java
@@ -0,0 +1,12 @@
+package com.snappad.snappad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnappadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnappadApplication.class, args);
+	}
+}
