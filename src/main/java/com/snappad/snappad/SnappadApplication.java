7d9f0c1dc0d5d5c5928d4d01900db76d2d9c2343
Fatemeh <fatemehashemi75>
2018-11-13 08:37:33 -0800

    bus commit

--- src/main/java/com/snappad/snappad/SnappadApplication.java
+++ src/main/java/com/snappad/snappad/SnappadApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SnappadApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SnappadApplication.class, args);
 	}
