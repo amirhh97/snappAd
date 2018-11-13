6cabc00fe25fea03b7bcf4e030c71e1ce0c2a791
Fatemeh <fatemehashemi75>
2018-11-13 04:30:04 -0800

    first commit from fatemeh

--- src/main/java/com/snappad/snappad/SnappadApplication.java
+++ src/main/java/com/snappad/snappad/SnappadApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SnappadApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SnappadApplication.class, args);
 	}
 }
